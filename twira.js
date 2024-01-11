f776e4ed17a63cf655d47af19e4359f53761f4a3
munyi-victor <munyivictor6@gmail.com>
2024-01-11 19:57:12 +0300

    added notification and message sections

--- twira.js
+++ twira.js
@@ -5,3 +5,15 @@ function more() {
 function hide() {
   document.getElementById("more").style.display = "none";
 }
+
+function check() {
+  document.querySelector(".not").style.backgroundColor = "grey";
+}
+
+function showMsg() {
+  if (document.getElementById("msg").style.top === "45vh") {
+    document.getElementById("msg").style.top = "92vh";
+  } else {
+    document.getElementById("msg").style.top = "45vh";
+  }  
+}
